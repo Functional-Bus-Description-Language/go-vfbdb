df669b4683115d591891f829632fc563642c98c7
Michał Kruszewski <mkru@protonmail.com>
2022-01-27 11:03:32 +0100

    Add single continuous 2 regs test case for func.

---
+++ tests/co-simulations/func/single_continuous_2_regs_no_returns/python/tb.py
@@ -0,0 +1,37 @@
+import random
+import sys
+import traceback
+
+from cosim_interface import CosimInterface
+import wbfbd
+
+WRITE_FIFO_PATH = sys.argv[1]
+READ_FIFO_PATH = sys.argv[2]
+
+
+try:
+    print("\nstarting cosimulation")
+
+    cosim_interface = CosimInterface(WRITE_FIFO_PATH, READ_FIFO_PATH)
+
+    Main = wbfbd.Main(cosim_interface)
+
+    s = random.randint(0, 2 ** 16 - 1)
+    c = random.randint(2**33, 2 ** 40 - 1)
+
+    print(f"Calling add function, s = {s}, c = {c}")
+    Main.Add(s, c)
+
+    print(f"Reading result")
+    result = Main.Result.read()
+
+    if s + c != result:
+        print(f"Wrong result, got {result}, expecting {s+c}")
+        cosim_interface.end(1)
+
+    print("\nending cosimulation")
+    cosim_interface.end(0)
+
+except Exception as E:
+    cosim_interface.end(1)
+    print(traceback.format_exc())
